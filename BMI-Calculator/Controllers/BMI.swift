89316d58497584ffd24917ed4050abdd825eec41
Adilet Abdukarim <adiletabdukarim96@gmail.com>
2021-06-23 14:30:38 +0900

    First commit

---
+++ BMI-Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Adilet Abdukarim on 2021/05/27.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    var value: Float
+    var advice: String
+    var color: UIColor
+}
